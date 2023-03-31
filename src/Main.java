34d1395f4548acbe25dbe56b3a6a384d00ddc238
Ridvan <cadiroglu34@gmail.com>
2023-03-31 17:15:08 -0500

    the lights have been added

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,17 @@ public class Main {
         audi.color="black";
         audi.model="A3";
         audi.power="5000cc";
+
+        Engine X=new Engine();
+
+        X.cylinders="8";
+        X.size="1234cc";
+        X.weight="6435k";
+
+        lights neon=new lights();
+        neon.color="red";
+        neon.size="15cm";
+        neon.intensity="2434343lumin";
+        
     }
 }
